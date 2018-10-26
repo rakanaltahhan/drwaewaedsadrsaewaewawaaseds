40a211a1f669828aebeb25f9473a43db30e1b161
iiTW12-Toxic <44296866+iiTW12-Toxic@users.noreply.github.com>
2018-10-26 02:21:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1041,14 +1041,14 @@ client.on('message', message => {
        if (message.content ===   prefix + "add role") {
     message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما امرت  *** ")
     message.guild.members.forEach(m => {
-    m.addRole(message.guild.roles.find('name', 'اسم الرتبة'))
+    m.addRole(message.guild.roles.find('name', '- TS'))
     })
     }
     
        if (message.content ===   prefix + "remove role") {
     message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما امرت  *** ")
     message.guild.members.forEach(m => {
-    m.removeRole(message.guild.roles.find('name', 'اسم الرتبة'))
+    m.removeRole(message.guild.roles.find('name', '- TS'))
     })
     }
     
