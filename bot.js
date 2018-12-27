8a83a4f571a6c47e0b0695fa0ae79089bbda39f2
rakanaltahhan <44034112+rakanaltahhan@users.noreply.github.com>
2018-12-27 19:02:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const prefix = '^'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` ^help | for help `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
